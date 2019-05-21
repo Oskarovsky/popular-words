33fe9f3376d303b3e3793f72711cd5f56638d0a2
oskarro <oskar.slyk@gmail.com>
2019-05-21 12:28:03 +0200

    1k most popular words with comparision Closes #4 #5

--- src/main/java/pl/sii/PopularWords.java
+++ src/main/java/pl/sii/PopularWords.java
@@ -1,22 +1,25 @@
 package pl.sii;
 
-import org.apache.commons.lang3.NotImplementedException;
-
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.*;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toMap;
 
 public class PopularWords {
 
     private final String FILE_PATH = (getClass().getResource("/3esl.txt")).getPath();
     private static final String REGEX = "([,.\\s]+)";
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException
+    {
         PopularWords popularWords = new PopularWords();
         Map<String, Long> result = popularWords.findOneThousandMostPopularWords();
         result.entrySet().forEach(System.out::println);
     }
+
 
     private List<String> extractWordsFrom(String filePath) throws IOException
     {
@@ -36,7 +39,20 @@ public class PopularWords {
         return words;
     }
 
-    public Map<String, Long> findOneThousandMostPopularWords() {
-        throw new NotImplementedException("TODO implementation");
+
+
+    Map<String, Long> findOneThousandMostPopularWords() throws IOException
+    {
+        return extractWordsFrom(FILE_PATH)
+                .stream()
+                .collect(Collectors.toConcurrentMap(word -> word, word -> 1L, Long::sum))
+                .entrySet()
+                .stream()
+                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                .limit(1000)
+                .collect(toMap(Map.Entry::getKey,
+                        Map.Entry::getValue,
+                        (v1, v2) -> { throw new IllegalStateException(); },
+                        LinkedHashMap::new));
     }
 }
--- src/test/java/pl/sii/PopularWordsTest.java
+++ src/test/java/pl/sii/PopularWordsTest.java
@@ -3,18 +3,27 @@ package pl.sii;
 import org.apache.commons.lang3.NotImplementedException;
 import org.junit.Test;
 
+import java.io.File;
+import java.io.IOException;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.net.URL;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
+import static java.lang.Long.parseLong;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 
 public class PopularWordsTest {
     private static final PopularWords testee = new PopularWords();
+    private final String FILE_PATH = (getClass().getResource("/all.num")).getPath();
+    private static final String REGEX = " ";
+
 
     @Test
-    public void shouldReturnOneThousandMostPopularWords() {
+    public void shouldReturnOneThousandMostPopularWords() throws IOException {
         //given
         Map<String, Long> wordsFrequencyListCreatedByAdamKilgarriff = getWordsFrequencyListCreatedByAdamKilgarriff();
 
@@ -35,9 +44,16 @@ public class PopularWordsTest {
 
         result.forEach((key, value) -> {
             BigDecimal valueUsagePercentage = calculatePercentage(value, totalFrequencyInAResult);
-            BigDecimal kilgarriffUsagePercentage = calculatePercentage(wordsFrequencyListCreatedByAdamKilgarriff.get(key), totalFrequencyByKilgarriff);
-            BigDecimal diff = kilgarriffUsagePercentage.subtract(valueUsagePercentage);
-            System.out.println(key + "," + valueUsagePercentage + "%," + kilgarriffUsagePercentage + "%," + (new BigDecimal(0.5).compareTo(diff.abs()) > 0) + " " + diff);
+            try {
+                BigDecimal kilgarriffUsagePercentage = calculatePercentage(wordsFrequencyListCreatedByAdamKilgarriff.get(key), totalFrequencyByKilgarriff);
+                BigDecimal diff = kilgarriffUsagePercentage.subtract(valueUsagePercentage);
+                System.out.println(key + "," + valueUsagePercentage + "%," + kilgarriffUsagePercentage + "%," + (new BigDecimal(0.5).compareTo(diff.abs()) > 0) + " " + diff);
+            } catch (NullPointerException ex) {
+                BigDecimal kilgarriffUsagePercentage = calculatePercentage(0, totalFrequencyByKilgarriff);
+                BigDecimal diff = kilgarriffUsagePercentage.subtract(valueUsagePercentage);
+                System.out.println(key + "," + valueUsagePercentage + "%," + kilgarriffUsagePercentage + "%," + (new BigDecimal(0.5).compareTo(diff.abs()) > 0) + " " + diff);
+            }
+
         });
     }
 
@@ -45,7 +61,23 @@ public class PopularWordsTest {
         return BigDecimal.valueOf(obtained * 100 / total).setScale(4, RoundingMode.HALF_UP);
     }
 
-    private Map<String, Long> getWordsFrequencyListCreatedByAdamKilgarriff() {
-        throw new NotImplementedException("TODO implementation");
+    private Map<String, Long> getWordsFrequencyListCreatedByAdamKilgarriff() throws IOException {
+        Map<String, Long> wordsFrequency = new HashMap<>();
+
+        try (Scanner scanner = new Scanner(new File(FILE_PATH))) {
+            scanner.nextLine();
+            while (scanner.hasNextLine()) {
+                String[] parts = scanner.nextLine().split(REGEX);
+                if (wordsFrequency.get(parts[1]) != null) {
+                    wordsFrequency.put(parts[1], wordsFrequency.get(parts[1]) + parseLong(parts[0]));
+                } else {
+                    wordsFrequency.put(parts[1], parseLong(parts[0]));
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return wordsFrequency;
     }
 }
