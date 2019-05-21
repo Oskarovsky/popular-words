ec4cfbdd904e201c6d430daf7af99128f598d906
oskarro <oskar.slyk@gmail.com>
2019-05-21 12:06:50 +0200

    Added static vars

--- src/main/java/pl/sii/PopularWords.java
+++ src/main/java/pl/sii/PopularWords.java
@@ -6,6 +6,9 @@ import java.util.Map;
 
 public class PopularWords {
 
+    private final String FILE_PATH = (getClass().getResource("/3esl.txt")).getPath();
+    private static final String REGEX = "([,.\\s]+)";
+
     public static void main(String[] args) {
         PopularWords popularWords = new PopularWords();
         Map<String, Long> result = popularWords.findOneThousandMostPopularWords();
